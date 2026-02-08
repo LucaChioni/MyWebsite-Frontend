ae3a7407955f05ed5657bf80b3ed5bffadd78809
Luca Chioni <luca.chioni46@gmail.com>
2026-02-08 12:41:38 +0100

    Messo testo temporaneo nella sezione sviluppo videogiochi (per rilascio)

--- js/i18n/pages/game_dev.js
+++ js/i18n/pages/game_dev.js
@@ -3,8 +3,10 @@ import "../core.js";
 window.i18nRegister({
     it: {
         game_dev_title: "Sviluppo videogiochi",
+        game_dev_subtitle: "Devo ancora finire di scrivere questa parte.<br />È stato un bel periodo comunque...",
     },
     en: {
         game_dev_title: "Game development",
+        game_dev_subtitle: "I still have to finish writing this part.<br />Anyway, it was a good period...",
     },
 });
